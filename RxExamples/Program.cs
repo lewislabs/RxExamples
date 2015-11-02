ab297e0c472a55afebdf70462fdc26ab28b9b6ec
Christopher Lewis <chris.lewis@hudl.com>
2015-11-02 15:11:34 +0000

    A Scan example

--- RxExamples/Program.cs
+++ RxExamples/Program.cs
@@ -8,31 +8,11 @@ namespace RxExamples
         static void Main(string[] args)
         {
             var observable = new MyRandomObservable();
-            var disposable1= observable
-                .Where(i=>i%2==0)
-                .Subscribe(i =>
-                {
-                    Console.Out.WriteLine($"Subscription 1 - Got {i}");
-                });
-            var disposable2 = observable
-                .Skip(5)
-                .Subscribe(i =>
-                {
-                    Console.Out.WriteLine($"Subscription 2 - Got {i}");
-                });
-            var disposable3 = observable
-                .FirstAsync()
-                .Subscribe(i =>
-                {
-                    Console.Out.WriteLine($"Subscription 3 - Got {i}");
-                }, () =>
-                {
-                    Console.Out.WriteLine("I'm Done!");
-                });
+            var disposable = observable
+                .Scan(0, (acc, current) => acc + current)
+                .Subscribe(i => Console.Out.WriteLine($"Running Total: {i}"));
             Console.ReadKey();
-            disposable1.Dispose();
-            disposable2.Dispose();
-            disposable3.Dispose();
+            disposable.Dispose();
             Console.ReadKey();
         }
     }

54b2e395176b7ba6264c6cead2afbaf370673562
Christopher Lewis <chris.lewis@hudl.com>
2015-11-02 14:38:50 +0000

    A couple of linq style subscriptions

--- RxExamples/Program.cs
+++ RxExamples/Program.cs
@@ -8,11 +8,31 @@ namespace RxExamples
         static void Main(string[] args)
         {
             var observable = new MyRandomObservable();
-            var disposable = observable
+            var disposable1= observable
                 .Where(i=>i%2==0)
-                .Subscribe(new MyRandomObserver());
+                .Subscribe(i =>
+                {
+                    Console.Out.WriteLine($"Subscription 1 - Got {i}");
+                });
+            var disposable2 = observable
+                .Skip(5)
+                .Subscribe(i =>
+                {
+                    Console.Out.WriteLine($"Subscription 2 - Got {i}");
+                });
+            var disposable3 = observable
+                .FirstAsync()
+                .Subscribe(i =>
+                {
+                    Console.Out.WriteLine($"Subscription 3 - Got {i}");
+                }, () =>
+                {
+                    Console.Out.WriteLine("I'm Done!");
+                });
             Console.ReadKey();
-            disposable.Dispose();
+            disposable1.Dispose();
+            disposable2.Dispose();
+            disposable3.Dispose();
             Console.ReadKey();
         }
     }
